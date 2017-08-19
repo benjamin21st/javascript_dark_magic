d5cc97bb4e20971cd0c21b57a4010a7eb70a43f4
Benjamin Zhang <bzhang7@outlook.com>
2017-08-19 00:05:03 -0400

    updated the extend function, making it more versatile

--- parasitic_combination_inheritance.js
+++ parasitic_combination_inheritance.js
@@ -5,28 +5,51 @@
  */
 'use strict';
 
+/**
+ * A helper function for us to extend a SubType with SuperType's features
+ * @param {*} SubType
+ * @param {*} SuperType
+ */
+function extend(SubType, SuperType) {
+  var prototype = SuperType.prototype;
+  prototype.constructor = SubType;
+  // Instead of taking prototype all in, we should allow SubType's prototype to
+  // override SuperType's
+  for (var key in prototype) {
+    if (!(key in SubType.prototype)) {
+      SubType.prototype[key] = prototype[key];
+    }
+  }
+  // SubType.prototype = prototype;
+}
+
+/**
+ * Defines our example SuperType
+ * @param {*} name
+ */
 function Person(name) {
   this.name = name;
   this.languages = ['English'];
 }
 
+/**
+ * A method we expect our SubType to inherit, and that this function should be the same
+ * one as the inheritted, meaning no new functions should be redefined when creating
+ * the subType
+ */
 Person.prototype.greet  = function () {
   return 'Hi, my name is ' + this.name + '.';
 }
 
+/**
+ * Our example subType
+ * @param {*} name
+ * @param {*} languages
+ */
 function Developer(name, languages) {
   Person.call(this, name); // or apply(this, [name]) if you are uncertain of how many params it takes
   this.languages = languages;
 }
-
-function extend(SubType, SuperType) {
-  var prototype = SuperType.prototype;
-  prototype.constructor = SubType;
-  SubType.prototype = prototype;
-}
-
-extend(Developer, Person);
-
 
 Developer.prototype.intro = function () {
   var lang = '';
@@ -38,6 +61,8 @@ Developer.prototype.intro = function () {
   }
   return this.greet() + ' I write ' + lang;
 }
+
+extend(Developer, Person);
 
 // Tests
 var p = new Person('p');
