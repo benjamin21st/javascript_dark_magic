895e9262c487ca5df752cc79840633c00e764381
Benjamin Zhang <bzhang7@outlook.com>
2017-08-23 06:51:57 -0400

    added an example of compiled typescript implementation of inheritance

---
+++ oop/compiled_inheritance_demo_from_typescript.js
@@ -0,0 +1,58 @@
+/**
+ * This is the compiled code from typescript that demonstrates inheritance, the
+ * advantage of this (for those who cannot use TypeScript or ES6) is that it keeps
+ * the "classes" seperate and encapsulated, so you wouldn't have a whole bunch of
+ * A.prototype.func all over the place.
+ */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Animal = (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    Animal.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 0; }
+        console.log(this.name + " moved " + distanceInMeters + "m.");
+    };
+    Animal.prototype.greet = function () {
+        alert(this.name);
+    };
+    return Animal;
+}());
+var Horse = (function (_super) {
+    __extends(Horse, _super);
+    function Horse(name) {
+        return _super.call(this, name) || this;
+    }
+    Horse.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 45; }
+        console.log("Galloping...");
+        _super.prototype.move.call(this, distanceInMeters);
+    };
+    return Horse;
+}(Animal));
+var Snake = (function (_super) {
+    __extends(Snake, _super);
+    function Snake(name) {
+        return _super.call(this, name) || this;
+    }
+    Snake.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 5; }
+        console.log("Slithering...");
+        _super.prototype.move.call(this, distanceInMeters);
+    };
+    return Snake;
+}(Animal));
+var sam = new Snake("Sammy the Python");
+var tom = new Horse("Tommy the Palomino");
+sam.move();
+tom.move(34);
+console.assert(sam.greet === tom.greet);
