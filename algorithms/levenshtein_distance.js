ca393721b6dae8a871343f07335a704da3c7ec3a
Benjamin Zhang <bzhang7@outlook.com>
2017-10-29 17:10:49 -0400

    new algorithm: compute levenshtein distance

---
+++ algorithms/levenshtein_distance.js
@@ -0,0 +1,37 @@
+let log = console.log;
+
+function levenshteinDistance (s, t) {
+  let cache = new Array(s.length);
+  for (let i = 0; i < s.length; ++i) {
+    cache[i] = (new Array(t.length)).fill(-1);
+  }
+  return levenshteinDistanceHelper (s, s.length - 1, t, t.length - 1, cache);
+}
+
+function levenshteinDistanceHelper (s, si, t, ti, cache) {
+  if (si < 0) { return ti + 1; }
+  else if (ti < 0) { return si + 1; }
+  else {
+    if (cache[si][ti] === -1) {
+      // calculate new cache item
+      if (s[si] === t[ti]) {
+        cache[si][ti] = levenshteinDistanceHelper(s, si - 1, t, ti - 1, cache);
+      } else {
+        let sub = levenshteinDistanceHelper(s, si - 1, t, ti - 1, cache);
+        let ins = levenshteinDistanceHelper(s, si, t, ti - 1, cache);
+        let del = levenshteinDistanceHelper(s, si - 1, t, ti, cache);
+        cache[si][ti] = Math.min(sub, ins, del) + 1;
+      }
+    }
+    return cache[si][ti];
+  }
+}
+
+console.log('Run: testLevenshteinDistance (s, t) to test');
+
+function testLevenshteinDistance (s, t) {
+  s = s || 'Saturday';
+  t = t || 'Sundays';
+  let res = levenshteinDistance(s, t);
+  console.log(res);
+}
