382fc7b84a255f3af9bfde79f249cb38b3361e30
Benjamin Zhang <bzhang7@outlook.com>
2017-10-29 18:37:35 -0400

    add new approach, recursive easy-to-understand, slow

--- algorithms/levenshtein_distance.js
+++ algorithms/levenshtein_distance.js
@@ -1,14 +1,14 @@
 let log = console.log;
 
-function levenshteinDistance (s, t) {
+function levenshteinDistance(s, t) {
   let cache = new Array(s.length);
   for (let i = 0; i < s.length; ++i) {
     cache[i] = (new Array(t.length)).fill(-1);
   }
-  return levenshteinDistanceHelper (s, s.length - 1, t, t.length - 1, cache);
+  return levenshteinDistanceHelper(s, s.length - 1, t, t.length - 1, cache);
 }
 
-function levenshteinDistanceHelper (s, si, t, ti, cache) {
+function levenshteinDistanceHelper(s, si, t, ti, cache) {
   if (si < 0) { return ti + 1; }
   else if (ti < 0) { return si + 1; }
   else {
@@ -27,9 +27,34 @@ function levenshteinDistanceHelper (s, si, t, ti, cache) {
   }
 }
 
+/**
+ * This is slow and I couldn't seem to figure out a way to optimize it.
+ * @param {string} s
+ * @param {string} t
+ */
+function levenshteinDistanceRec(s, t) {
+  return levenshteinDistanceRecHelper(s, t, 0);
+}
+
+function levenshteinDistanceRecHelper(s, t, idx) {
+  console.assert(s.substring(0, idx) === t.substring(0, idx));
+  if (idx >= s.length) return t.length - idx;
+  if (idx >= t.length) return s.length - idx;
+
+  let res;
+  if (s[idx] === t[idx]) {
+    return levenshteinDistanceRecHelper(s, t, idx + 1);
+  } else {
+    let sub = levenshteinDistanceRecHelper(s.substring(0, idx) + t[idx] + s.slice(idx + 1), t, idx + 1);
+    let ins = levenshteinDistanceRecHelper(s.substring(0, idx) + t[idx] + s.slice(idx), t, idx);
+    let del = levenshteinDistanceRecHelper(s.substring(0, idx) + s.slice(idx + 1), t, idx);
+    return Math.min(sub, ins, del) + 1;
+  }
+}
+
 console.log('Run: testLevenshteinDistance (s, t) to test');
 
-function testLevenshteinDistance (s, t) {
+function testLevenshteinDistance(s, t) {
   s = s || 'Saturday';
   t = t || 'Sundays';
   let res = levenshteinDistance(s, t);
